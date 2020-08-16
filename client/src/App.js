9817424bab84d185d1ea0106569568effcde62d5
André Sebastian-Bath <a.sebastian@posteo.de>
2020-08-16 13:56:37 +0200

    clean code

--- client/src/App.js
+++ client/src/App.js
@@ -6,31 +6,32 @@ import './App.css';
 function App() {
 
   let [data, setData] = useState([])
+  let scores = []
+  let noResult = false
 
   const fetchData = async () => {
-    let scores = []
-    let noResult = false
     let count = 1;
-
     while (!noResult) {
       await axios('https://new.scoresaber.com/api/player/76561198037132296/scores/recent/'+ count++)
         .then(scoreReply => {
-          console.log("fetchData -> scoreReply", scoreReply)
           if (scoreReply.ok) {
+            console.log("Scores:", scores)
             noResult = true;
-            console.log(scores)
-            return
+            // return scores
           }
           scores.push(...scoreReply.data.scores)
+          return scores
         })
     }
-    setData(scores)
   };
 
   useEffect(() => {
-    console.log("use Effect")
     fetchData()
-     .finally(() => console.log(data))
+     .finally((result) => {
+       console.log("App -> result", result)
+       console.log("Ergebnis: ", scores)
+       setData(scores)
+    })
   }, [])
 
   
@@ -38,9 +39,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>DATA:</h1>
+        <p>Länge: {data.length} </p>
+        {(data.length > 0) && data.map((item, i) => <p key={i}>{item.score}</p>)}
+        {(data.length <= 0) && <p>Loading...</p>}
       </header>
       <div>
-        {(data.length > 0) && data.map((item, i) => <p key={i}>item.score</p>)}
+
+
       </div>
     </div>
   );
