c7ed1e177ba14832a66613e9cecac3a7f059ba0b
André Sebastian <a.sebastian@posteo.de>
2020-08-16 17:20:04 +0200

    add  basic query funtcionality

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,12 @@ import './App.css';
 function App() {
 
   let [data, setData] = useState([])
+  let [query, setQuery] = useState(null)
   let [currentID, setCurrentID] = useState('76561198037132296')
   let scores = []
   let noResult = false
 
-  const fetchData = async () => {
+  const fetchData = async (currentID) => {
     let count = 1;
     while (!noResult) {
       await axios('https://new.scoresaber.com/api/player/'+ currentID +'/scores/recent/'+ count++)
@@ -23,20 +24,35 @@ function App() {
   };
 
   useEffect(() => {
-    fetchData().finally(() => setData(scores))
+    fetchData(currentID).finally(() => setData(scores))
   }, [])
 
+  useEffect(() => {
+    console.log("Data has changed")
+    setQuery(null)
+  }, [data])
+
+  function clickSubmit (e) {
+    console.log(query)
+    setCurrentID(query)
+    fetchData(query).finally(() => setData(scores))
+  }
+
+  function handleChange (e) {
+    setQuery(e.target.value)
+  }
   
   return (
     <div className="App">
       <header className="App-header">
+          <input onChange={e => handleChange(e)} type="number" />
+          <button onClick={clickSubmit}>Submit</button>
         <h1>DATA:</h1>
         <p>Länge: {data.length} </p>
         {(data.length > 0) && data.map((item, i) => <p key={i}>{item.score}</p>)}
         {(data.length <= 0) && <p>Loading...</p>}
       </header>
       <div>
-
 
       </div>
     </div>
