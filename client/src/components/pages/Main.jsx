9de567f03dc5aacd9dc92739dc68ff72ee7b5e93
André Sebastian <a.sebastian@posteo.de>
2020-08-17 13:30:51 +0200

    add login functionality

--- client/src/components/pages/Main.jsx
+++ client/src/components/pages/Main.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Dashboard from './Dashboard';
 import MyProfile from './MyProfile';
+import LoginBox from '../LoginBox';
 import '../../styles/pages/Main.scss'
+import api from '../../api';
 
-export default function Main() {
+function Main(props) {
+
+    let handleLogout = () => {
+        api.logout();
+        let userdata = { username: null, profilePic: null }
+        props.dispatch({ type: "UPDATE_USER_DATA", userdata })
+    }
+
     return (
         <div id="main">
-            <h1>Main</h1>
+            <div id="header">
+                <h1>Main</h1>
+                {props.username && <p>{props.username}</p>}
+                {!api.isLoggedIn() && <LoginBox />}
+                {api.isLoggedIn() && <p onClick={handleLogout}>Logout</p>}
+            </div>
             <Switch>
                 <Route path="/" exact component={Dashboard} />
                 <Route path="/myprofile" component={MyProfile} />
@@ -16,3 +31,12 @@ export default function Main() {
         </div>
     )
 }
+
+function mapStateToProps(reduxState){
+    return {
+      username: reduxState.username,
+      profilePic: reduxState.profilePic
+    }
+  }
+  
+  export default connect(mapStateToProps)(Main)
