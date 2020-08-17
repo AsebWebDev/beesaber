c3b9fe609ef53721eb34e7de3412c421feb6f97f
André Sebastian <a.sebastian@posteo.de>
2020-08-17 18:35:32 +0200

    remove unused code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,37 +1,9 @@
 const express = require("express")
-const passport = require('passport')
+// const passport = require('passport')
 const router = express.Router()
 const User = require("../models/User")
 
-// Bcrypt to encrypt passwords
-const bcrypt = require("bcrypt")
-const bcryptSalt = 10
-
-router.post("/login", (req, res, next) => {
-  const { username, password } = req.body
-
-//   User.findOne({ username })
-//     .then(userDoc => {
-//       if (!userDoc) {
-//         next(new Error("Incorrect username "))
-//         return
-//       }
-
-//       if (!bcrypt.compareSync(password, userDoc.password)) {
-//         next(new Error("Password is wrong"))
-//         return
-//       }
-
-//       req.logIn(userDoc, () => {
-//         userDoc.password = undefined
-//         res.json(userDoc)
-//       })
-//     })
-//     .catch(err => next(err))
-})
-
 router.post("/googlelogin", (req, res, next) => {
-  console.log("Google login hit")
   const { googleId, username, profilePic } = req.body
 
   User.findOne({ googleId })
@@ -51,32 +23,59 @@ router.post("/googlelogin", (req, res, next) => {
     .catch(err => next(err))
 })
 
-router.post('/login-with-passport-local-strategy', (req, res, next) => {
-  passport.authenticate('local', (err, theUser, failureDetails) => {
-    if (err) {
-      res.status(500).json({ message: 'Something went wrong' })
-      return
-    }
-
-    if (!theUser) {
-      res.status(401).json(failureDetails)
-      return
-    }
-
-    req.login(theUser, (err) => {
-      if (err) {
-        res.status(500).json({ message: 'Something went wrong' })
-        return
-      }
-
-      res.json(req.user)
-    })
-  })(req, res, next)
-})
-
 router.get("/logout", (req, res) => {
   req.logout()
   res.json({ message: 'You are out!' })
 })
 
 module.exports = router
+
+// Bcrypt to encrypt passwords
+// const bcrypt = require("bcrypt")
+// const bcryptSalt = 10
+
+// router.post("/login", (req, res, next) => {
+//   const { username, password } = req.body
+
+//   User.findOne({ username })
+//     .then(userDoc => {
+//       if (!userDoc) {
+//         next(new Error("Incorrect username "))
+//         return
+//       }
+
+//       if (!bcrypt.compareSync(password, userDoc.password)) {
+//         next(new Error("Password is wrong"))
+//         return
+//       }
+
+//       req.logIn(userDoc, () => {
+//         userDoc.password = undefined
+//         res.json(userDoc)
+//       })
+//     })
+//     .catch(err => next(err))
+// })
+
+// router.post('/login-with-passport-local-strategy', (req, res, next) => {
+//   passport.authenticate('local', (err, theUser, failureDetails) => {
+//     if (err) {
+//       res.status(500).json({ message: 'Something went wrong' })
+//       return
+//     }
+
+//     if (!theUser) {
+//       res.status(401).json(failureDetails)
+//       return
+//     }
+
+//     req.login(theUser, (err) => {
+//       if (err) {
+//         res.status(500).json({ message: 'Something went wrong' })
+//         return
+//       }
+
+//       res.json(req.user)
+//     })
+//   })(req, res, next)
+// })
