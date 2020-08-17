aaea0d67f791c614aa9931ccaf542835287c5030
André Sebastian <a.sebastian@posteo.de>
2020-08-17 13:29:48 +0200

    move api call from app to dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -8,55 +8,6 @@ import Main from './components/pages/Main';
 import './styles/pages/App.scss';
 
 function App(props) {
-  const { dispatch } = props;
-  let [data, setData] = useState([])
-  let [query, setQuery] = useState(null)
-  let [currentID, setCurrentID] = useState('76561198101951971')
-  let scores = []
-  let noResult = false
-
-  const fetchData = async (currentID) => {
-    let count = 1;
-    while (!noResult) {
-      await axios('https://new.scoresaber.com/api/player/'+ currentID +'/scores/recent/'+ count++)
-        .then(scoreReply => {
-          if (scoreReply.ok) noResult = true;
-          scores.push(...scoreReply.data.scores)
-          return scores
-        })
-    }
-  };
-
-  useEffect(() => {
-    fetchData(currentID).finally(() => setData(scores))
-  }, [])
-
-  useEffect(() => {
-    if (api.isLoggedIn() && api.getLocalStorageUser()) {
-      const { username, profilePic, _id } = api.getLocalStorageUser()
-      const userdata = { username, profilePic }
-      dispatch({ type: "UPDATE_USER_DATA", userdata })
-      // api.getUserSettings(_id)
-      // .then(settings => dispatch({ type: "UPDATE_USER_SETTINGS", settings}))
-      // .catch(err => dispatch(newNotification(err.toString())))
-    }
-  }, [dispatch])
-
-  useEffect(() => {
-    console.log("Data has changed")
-    setQuery(null)
-  }, [data])
-
-  function clickSubmit (e) {
-    console.log(query)
-    setCurrentID(query)
-    fetchData(query).finally(() => setData(scores))
-  }
-
-  function handleChange (e) {
-    setQuery(e.target.value)
-  }
-  
   return (
     <div id="App">
       <Menu />
--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios'
+import api from '../../api';
 
-export default function Dashboard() {
+function Dashboard(props) {
+    const { dispatch, myScoreSaberId } = props;
+    let [data, setData] = useState([])
+    let [query, setQuery] = useState(null)
+    let [myScores, setMyScores] = useState(null)
+    let [currentID, setCurrentID] = useState(myScoreSaberId ? myScoreSaberId : '76561198101951971')
+    
+    useEffect(() => {
+        if (api.isLoggedIn() && api.getLocalStorageUser()) {
+            const { username, profilePic, _id } = api.getLocalStorageUser()
+            const userdata = { username, profilePic, myScores }
+            props.dispatch({ type: "UPDATE_USER_DATA", userdata })
+            // api.getUserSettings(_id)
+            // .then(settings => dispatch({ type: "UPDATE_USER_SETTINGS", settings}))
+            // .catch(err => dispatch(newNotification(err.toString())))
+        }
+    }, [dispatch])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await api.getScores(currentID).then((scores) => setData(scores))
+        }
+        fetchData()
+    }, [])
+    
+    useEffect(() => {
+        console.log("Data has changed")
+        setQuery(null)
+    }, [data])
+    
+    function clickSubmit (e) {
+        console.log(query)
+        setCurrentID(query)
+        api.getScores(query).finally((scores) => setData(scores))
+    }
+    
+    function handleChange (e) {
+        setQuery(e.target.value)
+    }
+
     return (
         <div>
             <h1>Dashboard</h1>
+            <header className="App-header">
+                <input onChange={e => handleChange(e)} type="number" />
+                <button onClick={clickSubmit}>Submit</button>
+                <h1>DATA:</h1>
+                <p>Länge: {data.length} </p>
+                {(data.length > 0) && data.map((item, i) => <p key={i}>{item.score}</p>)}
+                {(data.length <= 0) && <p>Loading...</p>}
+            </header>
         </div>
     )
 }
+
+function mapStateToProps(reduxState){
+    return {
+      profilePic: reduxState.profilePic,
+      username: reduxState.username,
+      myScores: reduxState.myScores
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard);
