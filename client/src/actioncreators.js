b79cbe11966b2ff8a08fe1b1a7fef55dcf1742b7
André Sebastian <a.sebastian@posteo.de>
2020-08-17 11:33:24 +0200

    add update_user_data

--- client/src/actioncreators.js
+++ client/src/actioncreators.js
@@ -1,5 +1,6 @@
 export const GET_DATA = "GET_DATA";
 export const ADD_NOTIFICATION = "ADD_NOTIFICATION";
+export const UPDATE_USER_DATA = "UPDATE_USER_DATA";
 
 export function newNotification(message, typeOfNotification) {
   return {
