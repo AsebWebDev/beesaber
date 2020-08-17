d90b4891652f4b638ec9e3e0e1630a7dd94b2e2b
André Sebastian <a.sebastian@posteo.de>
2020-08-17 20:07:36 +0200

    protect page, access when logged in

--- client/src/components/pages/MyProfile.jsx
+++ client/src/components/pages/MyProfile.jsx
@@ -1,43 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import api from '../../api';
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon, MDBInput } from 'mdbreact';
 
 function MyProfile(props) {
     const { dispatch } = props;
+    // console.log("MyProfile ->  props.userdata",  props.userdata)
 
-    let [myScoreSaberId, setMyScoreSaberId] = useState((props.userdata.myScoreSaberId) ? props.userdata.myScoreSaberId : '777')
+    let [myScoreSaberId, setMyScoreSaberId] = useState((props.userdata) ? props.userdata.myScoreSaberId : '777')
 
     const handleChange = (e) => setMyScoreSaberId(e.target.value)
 
     const handleSave = () => {
         const userdata = { ...props.userdata, myScoreSaberId }
-        console.log("handleSave -> data", userdata)
+        console.log("handleSave -> userdata", userdata)
+        api.saveUserData(props.userdata._id, userdata)
         dispatch({ type: "UPDATE_USER_DATA", userdata })
     }
 
-    return (
-        <div>
-            <h1>My Profile</h1>
-            <MDBContainer>
-                <MDBRow>
-                    <MDBCol md="6">
-                    <form>
-                        <div className="grey-text">
-                            <MDBInput onChange={e => handleChange(e)} value={myScoreSaberId} label="Your ScoreSaber ID" icon="user" group type="text" validate error="wrong"
-                            success="right" />
-                        </div>
-                        <div className="text-center">
-                        <MDBBtn onClick={handleSave} outline color="secondary">
-                            Save
-                            <MDBIcon far icon="paper-plane" className="ml-1" />
-                        </MDBBtn>
-                        </div>
-                    </form>
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
-        </div>
-    )
+    if ( api.isLoggedIn() ) {
+        return (
+            <div>
+                <h1>My Profile</h1>
+                <MDBContainer>
+                    <MDBRow>
+                        <MDBCol md="6">
+                        <form>
+                            <div className="grey-text">
+                                <MDBInput onChange={e => handleChange(e)} value={myScoreSaberId} label="Your ScoreSaber ID" icon="user" group type="text" validate error="wrong"
+                                success="right" />
+                            </div>
+                            <div className="text-center">
+                            <MDBBtn onClick={handleSave} outline color="secondary">
+                                Save
+                                <MDBIcon far icon="paper-plane" className="ml-1" />
+                            </MDBBtn>
+                            </div>
+                        </form>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+            </div>
+        )
+    } else {
+        props.history.push("/") // Redirect to the main page
+        return null
+    }
+    
 }
 
 
