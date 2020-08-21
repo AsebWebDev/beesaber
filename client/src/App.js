dd5e7e8737d92aff7f744cf4e671c9f9597c89d4
André Sebastian <a.sebastian@posteo.de>
2020-08-21 22:23:29 +0200

    move Fetch request to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,17 +9,52 @@ import './styles/pages/App.scss';
 
 function App(props) {
   const { dispatch } = props;
+  const myScoreSaberId = (props.userdata) ? props.userdata.myScoreSaberId : null;
+  // let scoresRecent = (props.userdata.scoreData) ? props.userdata.scoreData.scoresRecent : null
 
+  const fetchData = async () => {
+    let scoreDataExist = (props.userdata.scoreData.scoreRecent && props.userdata.scoreData.scoreRecent.length > 0)
+    let dataUpdateNeeded = false; 
+
+    // check if Database latest Score is different from Scoresaber...
+    await api.dataUpdateNeeded(props.userdata.scoreData.scoresRecent[0], props.userdata.myScoreSaberId)
+        .then(result => {
+          console.log("Data refresh needed?: ", result)
+          dataUpdateNeeded = result
+        })
+    //... if yes / or no Data ever has been fetched, get all Scores
+    if (dataUpdateNeeded || !scoreDataExist) await api.getScores(myScoreSaberId).then((scoresRecent) => {
+        const scoresTop = [...scoresRecent] 
+        scoresTop.sort((a,b) => b.score - a.score )
+        const userdata = { ...props.userdata, scoreData: { scoresRecent, scoresTop } }
+        api.saveUserData(props.userdata._id, userdata)
+        dispatch({ type: "UPDATE_USER_DATA", userdata })
+    })
+  }
+
+  // GET BASIC USERDATA FROM BACKEND DATABASE
   useEffect(() => {
     if (api.isLoggedIn() && api.getLocalStorageUser()) {
       const { _id } = api.getLocalStorageUser()
       api.getUserData(_id)
         .then(userdata => {
-        console.log("App -> userdata", userdata)
           dispatch({ type: "UPDATE_USER_DATA", userdata })
         })
     }
   }, [dispatch])
+
+  // GET SCORES FROM SCORESABER 
+  useEffect(() => {
+      if (myScoreSaberId) fetchData(myScoreSaberId)
+  }, [myScoreSaberId])
+
+  // CHECK IF REFRESH IS NEEDED TODO: Use this function to check with database
+  // useEffect(() => {
+  //   if (scoresRecent) {
+  //     api.dataUpdateNeeded(props.userdata.scoreData.scoresRecent[0], props.userdata.myScoreSaberId)
+  //       .then(result => console.log("Data refresh needed?: ", result))
+  //   }
+  // }, [props.userdata.scoreData])
 
   return (
     <div id="App">
