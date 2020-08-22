2f008b93b9dec25c0578cc8d1d77d6e690e0d1f4
André Sebastian-Bath <a.sebastian@posteo.de>
2020-08-22 20:14:03 +0200

    semantic renaming of toggle function

--- client/src/components/pages/MyFriends.jsx
+++ client/src/components/pages/MyFriends.jsx
@@ -7,13 +7,13 @@ import api from '../../api';
 function MyFriends(props) {
     let [modal, setModal] = useState(false);
 
-    const handleSave = () => setModal(!modal)
+    const toggleModal = () => setModal(!modal)
 
     if ( api.isLoggedIn() ) {
         return (
             <div>
                 <h1>My Friends</h1>
-                <MDBBtn onClick={handleSave} outline color="success">
+                <MDBBtn onClick={toggleModal} outline color="success">
                     Add a friend
                     <MDBIcon icon="plus" className="ml-1" />
                 </MDBBtn>
@@ -23,7 +23,7 @@ function MyFriends(props) {
                 {props.userdata.friends && <p>No friends yet</p>}
 
                 {/* // MODAL ADD FRIENDS //  */}
-                {modal && <AddFriendModal handleSave={handleSave}/>}
+                {modal && <AddFriendModal toggleModal={toggleModal}/>}
 
             </div>
         )
