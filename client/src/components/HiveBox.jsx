d267738529c2eb3dcf597a2e5b39dc60689a6e49
André Sebastian-Bath <a.sebastian@posteo.de>
2020-08-25 14:45:32 +0200

    add HiveBox Component

---
+++ client/src/components/HiveBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/HiveBox.scss'
+
+export default function HiveBox() {
+    return (
+        <div id="hivebox">
+            HiveBox
+        </div>
+    )
+}
