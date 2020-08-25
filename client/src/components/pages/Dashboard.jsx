072f7e0c8910fddc71bec73583202cf120e6be53
André Sebastian-Bath <a.sebastian@posteo.de>
2020-08-25 11:50:43 +0200

    use Spinner and rerender on userdata change

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import ScoreBox from "../ScoreBox";
+import Spinner from "../Spinner";
 
 function Dashboard(props) {
     const { scoreData } = props.userdata;
     const myScoreSaberId = (props.userdata) ? (props.userdata.myScoreSaberId) : null;
 
+    useEffect(() => {
+        console.log("Dashboard useeffect")
+    }, [props.userdata])
+
     return (
-        <div>
+        <div id="dashboard">
             <h1>Dashboard</h1>
             <header className="App-header">
                 {myScoreSaberId &&
                     <div>
+                        
                         {scoreData && scoreData.scoresRecent &&
                             <div>
                                 <ScoreBox data={scoreData.scoresRecent} />
                                 <br/>
                                 <br/>
                             </div>}
-                        {scoreData && !scoreData.scoresRecent && <p>Loading...</p>}
+                        {scoreData && !scoreData.scoresRecent && < Spinner text="Loading..." />}
                     </div>}
                 {!myScoreSaberId && <p>No ID provided.</p>}
             </header>
