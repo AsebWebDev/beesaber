8895f9c79115e02ded10714dd58efbc0381a8dd1
André Sebastian-Bath <a.sebastian@posteo.de>
2020-08-25 17:32:37 +0200

    add description

--- client/src/components/HiveBox.jsx
+++ client/src/components/HiveBox.jsx
@@ -3,8 +3,9 @@ import '../styles/HiveBox.scss'
 
 export default function HiveBox() {
     return (
-        <div id="hivebox">
-            HiveBox
+        <div id="hivebox" className="card-container">
+            <h3>HiveBox</h3>
+            <p>Small List of Friends, like Scores, Pagination</p>
         </div>
     )
 }
