93708f2b18b3d73a705b06d282caa19931736a4e
André Sebastian-Bath <a.sebastian@posteo.de>
2020-08-30 14:02:37 +0200

    add todo

--- client/src/actioncreators.js
+++ client/src/actioncreators.js
@@ -2,6 +2,7 @@ export const ADD_NOTIFICATION = "ADD_NOTIFICATION";
 export const UPDATE_USER_DATA = "UPDATE_USER_DATA";
 
 export function newNotification(message, typeOfNotification) {
+  // TODO: format Notifications
   return {
     type: ADD_NOTIFICATION,
     notification: message,
