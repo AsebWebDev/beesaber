a510be106b1b72fe385b676447ef928163114e9b
André Sebastian-Bath <a.sebastian@posteo.de>
2020-08-30 17:18:22 +0200

    add setFetchStatus

--- client/src/actioncreators.js
+++ client/src/actioncreators.js
@@ -1,5 +1,6 @@
 export const ADD_NOTIFICATION = "ADD_NOTIFICATION";
 export const UPDATE_USER_DATA = "UPDATE_USER_DATA";
+export const SET_FETCH_STATUS = "SET_FETCH_STATUS";
 
 export function newNotification(message, typeOfNotification) {
   // TODO: format Notifications
@@ -7,5 +8,15 @@ export function newNotification(message, typeOfNotification) {
     type: ADD_NOTIFICATION,
     notification: message,
     typeOfNotification
+  }
+}
+
+export function setFetchStatus(status, text) {
+  return {
+    type: SET_FETCH_STATUS,
+    fetchingData: {
+      status,
+      text
+    }
   }
 }
