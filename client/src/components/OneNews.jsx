adba010e69d75c199619f05904b9e7aedc236508
André Sebastian-Bath <a.sebastian@posteo.de>
2020-08-31 23:06:55 +0200

    use DiffTags and Symbol Component + helper

--- client/src/components/OneNews.jsx
+++ client/src/components/OneNews.jsx
@@ -2,18 +2,26 @@ import React from 'react'
 import TimeAgo from 'react-timeago'
 import germanStrings from 'react-timeago/lib/language-strings/de'
 import buildFormatter from 'react-timeago/lib/formatters/buildFormatter'
+import { createNewsText } from "../helper/createNewsText";
+import DiffTags from './DiffTag'
+import Symbol from './Symbol'
 import '../styles/OneNews.scss'
 
 export default function OneNews(props) {
     const formatter = buildFormatter(germanStrings)
     const { text, type, song, date } = props.oneNews
     const difficulty = (song) ? song.difficulty : null
-
+    
     return (
         <div id="one-news">
-            {text}
-            <TimeAgo date={date} formatter={formatter} />
-            {difficulty}
+            <div className="card"> 
+                <div className="card-body">
+                    <h5 className="card-title">{type && <Symbol type={type}/>}New Score</h5>
+                    <p className="card-text">{createNewsText(props.oneNews)}</p>
+                    <a className="card-link"><DiffTags diff={difficulty} /></a>
+                    <a className="card-link"><TimeAgo date={date} formatter={formatter} /></a>
+                </div>
+            </div>
         </div>
     )
 }
