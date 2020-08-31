6450d2fc7c14170a327c7a5d634e2814ca7100af
André Sebastian-Bath <a.sebastian@posteo.de>
2020-08-31 20:31:45 +0200

    fix wrong indexes for difficulties

--- client/src/components/DiffTag.jsx
+++ client/src/components/DiffTag.jsx
@@ -5,11 +5,11 @@ export default function DiffTag(props) {
     let badgeColor, badgeText
 
     switch(props.diff) {
-        case '1': badgeColor = "badge badge-success"; badgeText="Easy"; break;
-        case '2': badgeColor = "badge badge-primary"; badgeText="Normal"; break;
-        case '3': badgeColor = "badge badge-warning"; badgeText="Hard"; break;
-        case '4': badgeColor = "badge badge-secondary"; badgeText="Expert"; break;
-        case '5': badgeColor = "badge badge-danger"; badgeText="Expert+"; break;
+        case 1: badgeColor = "badge badge-success"; badgeText="Easy"; break;
+        case 3: badgeColor = "badge badge-primary"; badgeText="Normal"; break;
+        case 5: badgeColor = "badge badge-warning"; badgeText="Hard"; break;
+        case 7: badgeColor = "badge badge-secondary"; badgeText="Expert"; break;
+        case 9: badgeColor = "badge badge-danger"; badgeText="Expert+"; break;
         default: badgeColor = "badge badge-default"; badgeText="Oops"; break;
     }
     
