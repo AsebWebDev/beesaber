aea5c5735c79309dc234cf84897f8a33d37706b6
André Sebastian-Bath <a.sebastian@posteo.de>
2020-08-31 20:04:26 +0200

    prepare News structure

--- client/src/components/OneNews.jsx
+++ client/src/components/OneNews.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import TimeAgo from 'react-timeago'
+import germanStrings from 'react-timeago/lib/language-strings/de'
+import buildFormatter from 'react-timeago/lib/formatters/buildFormatter'
+import '../styles/OneNews.scss'
 
 export default function OneNews(props) {
-    const { oneNews } = props
+    const formatter = buildFormatter(germanStrings)
+    const { text, type, song, date } = props.oneNews
+    const difficulty = (song) ? song.difficulty : null
+
     return (
         <div id="one-news">
-            {oneNews.text}
+            {text}
+            <TimeAgo date={date} formatter={formatter} />
+            {difficulty}
         </div>
     )
 }
