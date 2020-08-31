cef280ec9f2eb241102d774c9c78ce9e5f1eafde
André Sebastian-Bath <a.sebastian@posteo.de>
2020-08-31 19:44:29 +0200

    add OneNews Component and show last 5 news

--- client/src/components/NewsBox.jsx
+++ client/src/components/NewsBox.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import DiffTag from './DiffTag'
+import OneNews from './OneNews'
 
 import '../styles/NewsBox.scss'
 
 function NewsBox(props) {
-    const news = props.userdata.news.slice(0,5)
+    const numOfNews = 5
+    const news = props.userdata.news.slice(props.userdata.news.length-numOfNews,props.userdata.news.length).reverse() // show last 5 news
     return (
         <div id="newsbox" className="card-container">
             <h3>NewsBox</h3>
             <DiffTag diff="1"/>
-            {news.map(oneNews => <div>{oneNews.text}</div>)}
+            {news.map(oneNews => <OneNews oneNews={oneNews}/>)}
         </div>
-    )
+    ) 
 }
 
 function mapStateToProps(reduxState){
