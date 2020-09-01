f4372c0a8ee29d3e72d5e38befeb3edbab247ef9
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-01 18:39:27 +0200

    use className instead of class

--- client/src/components/BeeTag.jsx
+++ client/src/components/BeeTag.jsx
@@ -3,6 +3,6 @@ import '../styles/BeeTag.scss'
 
 export default function BeeTag(props) {
     return (
-        <span id="beetag" className="badge badge-warning"><i class="fab fa-forumbee" aria-hidden="true"></i>{props.userName}</span>
+        <span id="beetag" className="badge badge-warning"><i className="fab fa-forumbee" aria-hidden="true"></i>{props.userName}</span>
     )
 }
