c79971219b1caf5e480328d6a41bd4d30c87f59f
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-03 22:25:35 +0200

    fix href console warning and remove unused var

--- client/src/components/OneNews.jsx
+++ client/src/components/OneNews.jsx
@@ -10,7 +10,7 @@ import '../styles/OneNews.scss'
 
 export default function OneNews(props) {
     const formatter = buildFormatter(germanStrings)
-    const { text, type, song, date } = props.oneNews
+    const { type, song, date } = props.oneNews
     const difficulty = (song) ? song.difficulty : null
     
     return (
@@ -25,8 +25,8 @@ export default function OneNews(props) {
                     {type !== 'beatScore' && 
                     <h5 className="card-title">{type && <Symbol type={type}/>}<OneNewsText oneNews={props.oneNews} /></h5>}
                     
-                    {difficulty && <a className="card-link"><DiffTags diff={difficulty} /></a>}
-                    <a className="card-link"><TimeAgo date={date} formatter={formatter} /></a>
+                    {difficulty && <a href="/#" className="card-link"><DiffTags diff={difficulty} /></a>}
+                    <a href="/#" className="card-link"><TimeAgo date={date} formatter={formatter} /></a>
                 </div>
                 {song && <img className="pic-preview img-thumbnail" src={`https://new.scoresaber.com/api/static/covers/${song.songHash}.png`} alt="Cover"/>}
             </div>
