67aec16897763424836239c2ed31c184a754cee3
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-05 20:14:53 +0200

    add plus to add user and lift state

--- client/src/components/UserInfo.jsx
+++ client/src/components/UserInfo.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { MDBIcon } from 'mdbreact';
 import BeeTag from './BeeTag'
 import localeEmoji from 'locale-emoji';
 import avatar from '../media/251.jpg'
@@ -7,7 +8,7 @@ import '../styles/UserInfo.scss'
 export default class UserInfo extends Component {
 
     render() {
-        const { userInfoData } = this.props;
+        const { userInfoData, handleChose } = this.props;
         const url = ( userInfoData.avatar === '/images/steam.png' || userInfoData.avatar === '/images/oculus.png')
             ? avatar 
             : `https://new.scoresaber.com/api/static/avatars/${userInfoData.playerId}.jpg`
@@ -26,14 +27,13 @@ export default class UserInfo extends Component {
                     </thead>
                     <tbody>
                             <tr>
-                                <td>
-                                    {/* <img src={`https://new.scoresaber.com/api/static/avatars/${userInfoData.playerId}.jpg`} alt="Avatar"/> */}
-                                    <img src={url} alt="Avatar"/>
-                                </td>
+                                <td><img src={url} alt="Avatar"/></td>
                                 <td>{localeEmoji(`${userInfoData.country}`)}&nbsp;<BeeTag userName={userInfoData.playerName}/></td>
                                 <td>#{userInfoData.rank}</td>
                                 {userInfoData.countryRank && <td>#{userInfoData.countryRank}</td>}
                                 {userInfoData.totalPlayCount && <td>{userInfoData.totalPlayCount}</td>}
+                                {/* If there is handleChose exsting on props, there should be a 'plus' to add this user */}
+                                {handleChose && <td><p onClick={() => handleChose(userInfoData)}><MDBIcon icon="plus" className="ml-1" /></p></td>}
                             </tr>
                     </tbody>
                 </table>
