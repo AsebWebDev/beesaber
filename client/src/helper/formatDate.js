06f10dd9192b31211a62d0c86ae63565ef400a11
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-07 09:25:45 +0200

    remove helper function not needed anymore

--- client/src/helper/formatDate.js
+++
@@ -1,5 +0,0 @@
-export function formatDate(date) {
-    let d = new Date(date);
-    return ("0" + d.getDate()).slice(-2) + "/" + ("0" + (d.getMonth() + 1)).slice(-2) + "/" +
-        d.getFullYear() + " - " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
-};
