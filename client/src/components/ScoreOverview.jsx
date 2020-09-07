1c86189b2da858c7e3d0874410aaa9fbb05ac2d0
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-07 09:25:30 +0200

    integrate scoretabs

--- client/src/components/ScoreOverview.jsx
+++ client/src/components/ScoreOverview.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from 'react';
-import { MDBContainer, MDBTabPane, MDBTabContent, MDBNav, MDBNavItem, MDBNavLink } from "mdbreact";
+import { MDBContainer, MDBTabContent, MDBNav, MDBNavItem, MDBNavLink } from "mdbreact";
 import { connect } from 'react-redux';
-import { formatDate} from "../helper/formatDate";
+import ScoreTabs from './ScoreTabs'
+import '../styles/ScoreOverview.scss'
 
 function ScoreOverview(props) {
     let [activeItem, setActiveItem] = useState('1')
-
     const { dataRecent, dataTop } = props;
-    const difficulties = ['', 'Easy', '', 'Normal', '','Hard', '', 'Expert', '', 'Expert+'];
 
     const toggle = tab => {
         if (activeItem !== tab) setActiveItem(tab)
     };
 
     return (
-        <MDBContainer>
-                <h2>Score Overview:</h2>
+        <MDBContainer id="score-overview" className="score-box">
+                <h3>My Scores</h3>
                 <MDBNav className="nav-tabs mt-5">
                     <MDBNavItem>
                         <MDBNavLink link to="#" active={activeItem === 1} onClick={() => toggle('1')} role="tab" >
@@ -29,76 +28,8 @@ function ScoreOverview(props) {
                     </MDBNavItem>
                 </MDBNav>
                 <MDBTabContent activeItem={activeItem} >
-                    <MDBTabPane tabId="1" role="tabpanel">
-                        <p className="mt-2">
-                            <div className="col-md-10">
-                                <p>Recent Scores</p>
-                                <p>(count: {dataRecent.length})</p>
-                                <table className="table table-hover">
-                                    <thead>
-                                        <tr>
-                                        <th scope="col">Rank</th>
-                                        <th scope="col">Song</th>
-                                        <th scope="col">Level</th>
-                                        <th scope="col">Date - Time</th>
-                                        <th scope="col">Score</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {dataRecent.map((data, index) => {
-                                            return(
-                                            <tr key={index}>
-                                                <td>#{data.rank}</td>
-                                                <td>
-                                                <img src={`https://new.scoresaber.com/api/static/covers/${data.songHash}.png`} alt="Cover"/>
-                                                {data.songAuthorName} - {data.songName} by {data.levelAuthorName}
-                                                </td>
-                                                <td>{difficulties[data.difficulty]}</td>
-                                                <td>{formatDate(data.timeSet)}</td>
-                                                <td>{data.score}</td>
-                                                </tr>
-                                            )})
-                                        }
-                                    </tbody>
-                                </table>
-                            </div>
-                        </p>
-                    </MDBTabPane>
-                    <MDBTabPane tabId="2" role="tabpanel">
-                        <p className="mt-2">
-                            <div className="col-md-10">
-                                <p>Top Scores</p>
-                                <p>(count: {dataTop.length})</p>
-                                <table className="table table-hover">
-                                    <thead>
-                                    <tr>
-                                        <th scope="col">Rank</th>
-                                        <th scope="col">Song</th>
-                                        <th scope="col">Level</th>
-                                        <th scope="col">Date - Time</th>
-                                        <th scope="col">Score</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    {dataTop.map((data, index) => {
-                                        return(
-                                            <tr key={index}>
-                                                <td>#{data.rank}</td>
-                                                <td>
-                                                    <img src={`https://new.scoresaber.com/api/static/covers/${data.songHash}.png`} alt="Cover"/>
-                                                    {data.songAuthorName} - {data.songName} by {data.levelAuthorName}
-                                                </td>
-                                                <td>{difficulties[data.difficulty]}</td>
-                                                <td>{formatDate(data.timeSet)}</td>
-                                                <td>{data.score}</td>
-                                            </tr>
-                                        )})
-                                    }
-                                    </tbody>
-                                </table>
-                            </div>
-                        </p>
-                    </MDBTabPane>
+                    <ScoreTabs tabId="1" data={dataRecent} size="lg"/>
+                    <ScoreTabs tabId="2" data={dataTop} size="lg"/>
                 </MDBTabContent>
             </MDBContainer>
     )
---
+++ client/src/components/ScoreTabs.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { MDBTabPane, MDBBadge } from "mdbreact";
+import moment from 'moment'
+
+import DiffTags from './DiffTag'
+
+export default function ScoreTabs(props) {
+    const { tabId, data, size } = props
+    return (
+        <MDBTabPane tabId={tabId} role="tabpanel">
+            <p className="mt-2">
+                <div className="col-md-10">
+                    <p>Recent Scores</p>
+                    <p>(count: {data.length})</p>
+                    <table className="table table-hover">
+                        <thead>
+                            <tr>
+                            <th className="rank" scope="col">Rank</th>
+                            <th className="song" scope="col">Song</th>
+                            <th scope="col">Level</th>
+                            <th className="score" scope="col">Score</th>
+                            <th className="time" scope="col">Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((data, index) => {
+                                return(
+                                    <tr key={index}>
+                                        <td><MDBBadge color="pink">{data.rank}</MDBBadge></td>
+                                        <td>
+                                            <DiffTags diff={data.difficulty} />
+                                            <img src={`https://new.scoresaber.com/api/static/covers/${data.songHash}.png`} alt="Cover"/>
+                                            <MDBBadge color="dark">{data.songAuthorName} - {data.songName}<span className="greyed-out"> by {data.levelAuthorName}</span></MDBBadge>
+                                        </td>
+                                        <td><DiffTags diff={data.difficulty} /></td>
+                                        <td><MDBBadge color="orange">{data.score}</MDBBadge></td>
+                                        <td><span className="card-link blue-text"><b>{moment(data.timeSet).format('lll')}</b></span></td>
+                                    </tr>
+                                )})
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </p>
+        </MDBTabPane>
+    )
+}
