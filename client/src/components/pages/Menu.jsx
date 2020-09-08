c302d55b35615f9531a10087fe647f030eb3328d
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-08 18:16:15 +0200

    add sample animation

--- client/src/components/pages/Menu.jsx
+++ client/src/components/pages/Menu.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { MDBAnimation } from 'mdbreact';
 import api from '../../api';
 import '../../styles/pages/Menu.scss';
 
 function Menu() {
     return (
         <div id="menu">
-            <div id="brandlogo">
-                {/* neon honeycomb logo: */}
-                <div><span className="neon-yellow">I</span></div>
-                <div><span className="neon-yellow">I</span></div>
-                <div><span className="neon-yellow">I</span></div>
-                <div><span className="neon-yellow">I</span></div>
-                <div><span className="neon-yellow">I</span></div>
-                <div><span className="neon-yellow">I</span></div>
-                <div className="neon-red saber"></div>
-                <div className="neon-blue saber"></div>            
-            </div>
+            <MDBAnimation type="flip">
+                <div id="brandlogo">
+                    {/* neon honeycomb logo: */}
+                    <div><span className="neon-yellow">I</span></div>
+                    <div><span className="neon-yellow">I</span></div>
+                    <div><span className="neon-yellow">I</span></div>
+                    <div><span className="neon-yellow">I</span></div>
+                    <div><span className="neon-yellow">I</span></div>
+                    <div><span className="neon-yellow">I</span></div>
+                    <div className="neon-red saber"></div>
+                    <div className="neon-blue saber"></div>            
+                </div>
+            </MDBAnimation>
+            
             <div id="menu-main">
                 <h1 className="beesaber-title"><span className="neon-red">Bee</span><br></br><span className="neon-blue">Saber</span></h1>
                 <Link to="/"><span className="neon-red">Dashboard</span></Link>
