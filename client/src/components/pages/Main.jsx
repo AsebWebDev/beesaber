e3f7746f5e7b5523b752b5f9d9aa4a9b7bfa08b3
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-17 18:19:10 +0200

    fix typo

--- client/src/components/pages/Main.jsx
+++ client/src/components/pages/Main.jsx
@@ -25,7 +25,7 @@ function Main(props) {
                                         <div id="profile-login-icon">
                                             {userdata.profilePic && 
                                                 <img 
-                                                    referrerpolicy="no-referrer" //avoids broken img due to 403 response from google
+                                                    referrerPolicy="no-referrer" //avoids broken img due to 403 response from google
                                                     src={userdata.profilePic ? userdata.profilePic : profilePicPlaceholderUrl} 
                                                     id="profile-pic-sm" alt="profile pic"/>}
                                             {userdata.username}
