383ea6e22eea9eb8d38eb05540e6cf919226c40e
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-19 09:49:33 +0200

    use scss

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import './index.css';
+import './index.scss';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore } from 'redux';
