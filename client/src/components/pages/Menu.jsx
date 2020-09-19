42243b3fba72fe86e69e95b807d9f4ee0f271b54
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-19 14:58:15 +0200

    wrap menu points in div

--- client/src/components/pages/Menu.jsx
+++ client/src/components/pages/Menu.jsx
@@ -24,9 +24,11 @@ function Menu() {
             
             <div id="menu-main">
                 <h1 className="beesaber-title"><span className="neon-red">Bee</span><br></br><span className="neon-blue">Saber</span></h1>
-                <Link to="/"><span className="neon-red">Dashboard</span></Link>
-                {api.isLoggedIn() && <Link to="/myprofile"><span className="neon-red">My</span> <span className="neon-blue">Profile</span></Link>}
-                {api.isLoggedIn() && <Link to="/myhive"><span className="neon-red">My</span> <span className="neon-blue">Hive</span></Link>}
+                <div id="menu-points">
+                    <Link to="/"><span className="neon-red">Dashboard</span></Link>
+                    {api.isLoggedIn() && <Link to="/myprofile"><span className="neon-red">My</span> <span className="neon-blue">Profile</span></Link>}
+                    {api.isLoggedIn() && <Link to="/myhive"><span className="neon-red">My</span> <span className="neon-blue">Hive</span></Link>}
+                </div>   
             </div>
             {/* alternative logo made with svg: */}
             {/* <svg className="logo"><use xlinkHref="#logo-honeycomb" /></svg> */}
