d725452c02d176daf246d78c5b0aa15dc98b6c1e
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-19 16:29:14 +0200

    create own component for Google-Data

---
+++ client/src/components/GoogleProfileData.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import '../styles/GoogleProfileData.scss'
+import profilePicPlaceholderUrl from '../media/beesaberlogo.png'
+
+function GoogleProfileData(props) {
+    const { userdata, fetchingData } = props;
+
+    if (userdata) return (
+        <div id="profile-login-icon">
+            {userdata.profilePic &&     
+                <img 
+                    referrerPolicy="no-referrer" //avoids broken img due to 403 response from google
+                    src={userdata.profilePic ? userdata.profilePic : profilePicPlaceholderUrl} 
+                    id="profile-pic-sm" alt="profile pic"/>}
+            <span>{userdata.username}</span>
+        </div>                                                                  
+    )
+    else return null
+}
+
+function mapStateToProps(reduxState){
+    return {
+        userdata: reduxState.userdata,
+        fetchingData: reduxState.fetchingData
+    }
+  }
+  
+export default connect(mapStateToProps)(GoogleProfileData)
