cafa3f0b4b22a1493de204cf802d6c50298d8de9
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-21 17:47:52 +0200

    use div and flexbox grid instead of table

--- client/src/components/UserInfo.jsx
+++ client/src/components/UserInfo.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MDBBadge, MDBIcon  } from 'mdbreact';
+import { MDBBadge, MDBIcon, MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 import BeeTag from './BeeTag'
 import avatar from '../media/bee.jpg'
 import '../styles/UserInfo.scss'
@@ -14,41 +14,30 @@ export default class UserInfo extends Component {
             : `https://new.scoresaber.com/api/static/avatars/${userInfoData.playerId}.jpg`
         if (userInfoData) return (
             <div id="userinfo" className="col-md-12 card-container">
-                <table className="table table-sm">
-                   <thead>
-                       <tr>
-                            <th className="d-flex player">
-                                <div className="d-flex player-head left">
-                                    <BeeTag bee={userInfoData}/>
-                                </div>
-                                <div className="d-flex player-head right">
-                                    {rank && <MDBBadge color="light"><i className="fas fa-globe"></i>#{rank}</MDBBadge>}
-                                    {countryRank && <MDBBadge color="light">{country && <i className={country.toLowerCase() +" flag"}></i>}#{countryRank}</MDBBadge>}
-                                    <img src={url} alt="Avatar"/>
-                                </div>
-                                
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                            <tr className="d-flex">
-                                <td className="d-flex player">
-                                        {totalPlayCount && <span>Total Playcount: <MDBBadge color="light">{totalPlayCount}</MDBBadge></span>}
-                                        {pp && <span>PP: <MDBBadge color="light"> {pp}</MDBBadge></span>}
-                                </td>
-                                
-                                {/* If there is handleChose exsting on props, there should be a 'plus' to add this user */}
-                                {/* {handleChose && <td><p onClick={() => handleChose(userInfoData)}></p></td>} */}
-                                {handleChose && <p id="plusicon"><MDBIcon onClick={() => handleChose(userInfoData)} icon="plus-circle" /></p>}
-                            </tr>
-                            <tr className="d-flex">
-                                <td className="d-flex player">
-                                    {rank && <span><i className="fas fa-globe"/>-Rank: <MDBBadge color="light"> #{rank}</MDBBadge></span>}
-                                    <span></span>
-                                </td>
-                            </tr>
-                    </tbody>
-                </table>
+                <div className="table table-sm">
+                    <div id="userinfo-head">
+                        <div className="d-flex player-head left">
+                            <BeeTag bee={userInfoData}/>
+                        </div>
+                        <div className="d-flex player-head right">
+                            {rank && <MDBBadge color="light"><i className="fas fa-globe"></i>#{rank}</MDBBadge>}
+                            {countryRank && <MDBBadge color="light">{country && <i className={country.toLowerCase() +" flag"}></i>}#{countryRank}</MDBBadge>}
+                            <img src={url} alt="Avatar"/>
+                        </div>
+                    </div>
+                    <div id="userinfo-body">
+                        <MDBContainer>
+                            <MDBRow>
+                                <MDBCol size="7">{totalPlayCount && <span><MDBBadge color="light">{totalPlayCount}</MDBBadge> Total Playcount</span>}</MDBCol>
+                                <MDBCol size="5">{pp && <span><MDBBadge color="light"> {pp}</MDBBadge><p>PP</p></span>}</MDBCol>
+                            </MDBRow>
+                            <MDBRow>
+                                <MDBCol>{rank && <span><MDBBadge color="light"> #{rank}</MDBBadge><i className="fas fa-globe"/>-Rank</span>}</MDBCol>
+                            </MDBRow>
+                        </MDBContainer>
+                        {handleChose && <p id="plusicon"><MDBIcon onClick={() => handleChose(userInfoData)} icon="plus-circle" /></p>}
+                    </div>                              
+                </div>
             </div>
         ) 
         else return null;
