a4d3beca8046dbb735984de15387968e31e45365
André Sebastian-Bath <a.sebastian@posteo.de>
2020-09-25 22:39:43 +0200

    revamp beetag tooltip

--- client/src/components/BeeTag.jsx
+++ client/src/components/BeeTag.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
-import { MDBContainer, MDBTooltip } from "mdbreact";
+import { MDBContainer, MDBTooltip, MDBBadge } from "mdbreact";
+import avatarPlaceholder from '../media/bee.jpg'
 import '../styles/BeeTag.scss'
 
 export default function BeeTag(props) {
-    const { playerName, playerId } = props.bee
+    const { playerName, country, countryRank, rank, avatar, playerId } = props.bee
+    console.log("BeeTag -> props.bee", props.bee)
+    const url = ( avatar === '/images/steam.png' || avatar === '/images/oculus.png')
+            ? avatarPlaceholder 
+            : `https://new.scoresaber.com/api/static/avatars/${playerId}.jpg`
+
     return (
         <MDBContainer id="beetag" >
             <MDBTooltip domElement tag="span" placement="top">
                 <span className="tag badge badge-warning"><i className="fab fa-forumbee" aria-hidden="true"></i>{playerName}</span>
                 <div className="userInfoPopup">
-                    <ul>
-                    <li><b>{playerName}</b></li>
-                    <li>Id: {playerId}</li>
-                    </ul>
+                    <div id="beetag-header">
+                        <img src={url} alt="Avatar"/>
+                        <p><b>{playerName}</b></p>
+                    </div>
+                    <div id="beetag-tags" className="d-flex">
+                        {countryRank && <MDBBadge color="light">{country && <i className={country.toLowerCase() +" flag"}></i>}#{countryRank}</MDBBadge>}
+                        {rank && <MDBBadge color="light"><i className="fas fa-globe"/>#{rank}</MDBBadge>}
+                    </div>
                 </div>
             </MDBTooltip>
         </MDBContainer>
