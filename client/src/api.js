005ba953c89fa3c2d604df5a30e0d433cdf303b7
André Sebastian-Bath <a.sebastian@posteo.de>
2020-10-11 16:14:31 +0200

    comment code

--- client/src/api.js
+++ client/src/api.js
@@ -143,35 +143,42 @@ export default {
   },
 
   filterBeeIntersections(userdata) {
-    // TODO: Comment this part 
-    if (Object.keys(userdata).length !== 0 && userdata.constructor === Object) {
-        const myIntersections = [];
-        const newUserData = { ...userdata }
+    if (Object.keys(userdata).length !== 0 && userdata.constructor === Object) {            // check if Userdata is Object
+      const newUserData = { ...userdata }                                                   // make copy of original data  
+      const myIntersections = [];                                                           // prepare Array to fill later 
         const mySongs = newUserData.scoreData.scoresRecent
         const myBees = newUserData.bees
-        // ITERATE OVER ALL BEES
-        myBees.forEach((bee,i) => {
-          const beeSongs = bee.scoreData.scoresRecent
-          const doubles = []
-          const beeIntersections = []
-  
-          beeSongs.forEach(song => {
-            const songs = mySongs.filter(item => item.songHash === song.songHash)
-            if (songs.length === 1) {
-              beeIntersections.push({song: songs[0], bee})
-            } else if (songs.length > 1) {
-              songs.forEach(song => doubles.push({song, bee}))
+        // === ITERATE OVER ALL BEES ===
+        myBees.forEach((bee,i) => {                                                         // for each bee....
+          const beeSongs = bee.scoreData.scoresRecent                                       // ...get all their songs
+          const beeIntersections = []                                                       // ...prepare array for single intersections
+          const doubles = []                                                                // ...prepare array for intersection duplicates
+
+          // === ITERATE OVER ALL SONGS OF EACH BEE ===                                     
+          beeSongs.forEach(song => {                                                        // ... for each song of current bee
+            const songs = mySongs.filter(item => item.songHash === song.songHash)           // ... get all songs that user has also got in list (intersections)
+            if (songs.length === 1) {                                                       // if there is only one intersection...
+              beeIntersections.push({song: songs[0], bee})                                  // ... add it to prepared array
+            } else if (songs.length > 1) {                                                  // if there are more intersections, there will be duplicates (same hash, but different difficulties, that are counted twice)
+              songs.forEach(song => doubles.push({song, bee}))                              // ... add them to filter duplicates later
             } 
           })
 
-          const filteredDoubles = doubles.filter((elem, i, self) => i === self.findIndex((t) => t.song.songHash === elem.song.songHash && t.song.difficulty === elem.song.difficulty))
-          beeIntersections.push(...filteredDoubles)
-          myBees[i].beeIntersections = beeIntersections
-          myIntersections.push(...beeIntersections)
+          // AFTER ALL SONGS ARE SCANNED FOR INTERCEPTIONS 
+          
+          const filteredDoubles = doubles.filter(                     // ... remove duplicates
+            (elem, i, self) => i === self.findIndex((t) => t.song.songHash === elem.song.songHash && t.song.difficulty === elem.song.difficulty)
+          )
+          
+          beeIntersections.push(...filteredDoubles)                   // ... add the filtered intersections to the main array
+          myBees[i].beeIntersections = beeIntersections               // ... add the intersections of this specific bee to the bee data
+          myIntersections.push(...beeIntersections)                   // ... add the intersections of this specific bee to all my intersections
         })
-        newUserData.myIntersections = myIntersections
-        newUserData.bees = myBees
-      return newUserData
+
+        // AFTER ALL BEES ARE SCANNED FOR INTERCEPTIONS 
+        newUserData.myIntersections = myIntersections                 // ... add all my Intersections to new userdata that will be returned
+        newUserData.bees = myBees                                     // ... update all my bees in new userdata with their interceptions
+      return newUserData                                              // finaly return updated userdata
     } 
   },
 
