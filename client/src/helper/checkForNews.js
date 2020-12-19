bc4ecee91691c474e970b250452fddb84b1b6be3
André Sebastian-Bath <a.sebastian@posteo.de>
2020-12-19 21:35:19 +0100

    add console for debugging

--- client/src/helper/checkForNews.js
+++ client/src/helper/checkForNews.js
@@ -1,67 +1,87 @@
 // Function returns an Array of News
-import api from '../api';
-import News from '../prototypes/newsProto'
+import api from "../api";
+import News from "../prototypes/newsProto";
 
 export async function checkForNews(userdata) {
-    let bees = userdata.bees
-    const myScoreIds = userdata.scoreData.scoredSongsHashes
-    // myScoreIds.push(41918150) // FIXME: for testing
-    let news = [];
+  let bees = userdata.bees;
+  const myScoreIds = userdata.scoreData.scoredSongsHashes;
+  // myScoreIds.push(41918150) // FIXME: for testing
+  let news = [];
 
-    // CHECK FOR EACH BEE IN FRIENDS LIST
-    for (let i = 0; i < bees.length; i++) {
-        const { playerName, playerId } = bees[i]                    // get Playername and ID of Opponent
-        const oldTotalPlayCount = bees[i].totalPlayCount            // Get num of Games played from database 
-        await api.getScoreSaberUserInfo(playerId, 'id')             // FETCH USERDATA for current Bee
-            .then( userInfo => compareScores(userInfo, oldTotalPlayCount, playerId, playerName))
+  // CHECK FOR EACH BEE IN FRIENDS LIST
+  for (let i = 0; i < bees.length; i++) {
+    const { playerName, playerId } = bees[i]; // get Playername and ID of Opponent
+    const oldTotalPlayCount = bees[i].totalPlayCount; // Get num of Games played from database
+    await api
+      .getScoreSaberUserInfo(playerId, "id") // FETCH USERDATA for current Bee
+      .then((userInfo) =>
+        compareScores(userInfo, oldTotalPlayCount, playerId, playerName)
+      );
+  }
+
+  return { news, bees };
+
+  // FUNCTION TO COMPARE SCORES
+
+  async function compareScores(
+    userInfo,
+    oldTotalPlayCount,
+    playerId,
+    playerName
+  ) {
+    const newTotalPlayCount = userInfo ? userInfo.totalPlayCount : null; // Get Num of Games played from Scoresaber
+    if (newTotalPlayCount !== oldTotalPlayCount) {
+      // if different === new highscores exist
+      const numPlayedMore = newTotalPlayCount - oldTotalPlayCount; // calc the difference
+      if (userInfo)
+        console.log(userInfo.playerName, " played ", numPlayedMore, " more!");
+      await api
+        .getScores(playerId) // FETCH SCORES for current Bee
+        .then((scoreData) => {
+          if (scoreData && scoreData.scoresRecent) {
+            const { scoresRecent } = scoreData;
+            news.push(
+              new News({
+                // Create a News with changes data of current bee
+                text: `Your Bee ${userInfo.playerName} played ${numPlayedMore} new songs!`,
+                title: `Your Bee ${userInfo.playerName} played ${numPlayedMore} new songs!`,
+                bee: userInfo.playerName,
+                numPlayedMore,
+                songs: scoresRecent.slice(0, numPlayedMore),
+                type: "morePlayed",
+                date: new Date().toISOString(),
+              })
+            );
+            for (let j = 0; j < numPlayedMore; j++) {
+              // For each score....
+              const match = myScoreIds.includes(scoresRecent[j].scoreId); // check if song exist in our own Songlist
+              if (match) {
+                // if so...
+                const { songName, songAuthorName, score } = scoresRecent[j]; // grab Song Infos
+
+                // Get my score: filter my Songlist for same songId (can only be 1) and grab score
+                // const myScore = myScoreIds.filter(item => item.scoreId === scoresRecent[j].scoreId).score
+                const myScore = 279100; //FIXME: For testing...
+                if (score > myScore) {
+                  news.push(
+                    new News({
+                      text: `Your Bee ${playerName} beat you at ${songName} (${songAuthorName}): ${score} ( You: ${myScore}  ) `,
+                      type: "beatScore",
+                      bee: playerName,
+                      beeSocre: score,
+                      myScore,
+                      song: scoresRecent[j],
+                      date: scoresRecent[j].timeSet,
+                    })
+                  );
+                }
+              }
+            }
+          }
+          bees = bees.map((bee) =>
+            bee.playerId === playerId ? (bee = { ...userInfo, scoreData }) : bee
+          );
+        });
     }
-
-    return { news, bees }
-
-    // FUNCTION TO COMPARE SCORES
-
-    async function compareScores(userInfo, oldTotalPlayCount, playerId, playerName) {
-        const newTotalPlayCount = (userInfo) ? userInfo.totalPlayCount : null       // Get Num of Games played from Scoresaber
-        if (newTotalPlayCount !== oldTotalPlayCount) {                              // if different === new highscores exist
-            const numPlayedMore = newTotalPlayCount - oldTotalPlayCount             // calc the difference
-            if (userInfo) console.log(userInfo.playerName, " played ", numPlayedMore, " more!")
-            await api.getScores(playerId)                                           // FETCH SCORES for current Bee
-                .then(scoreData => {
-                    if (scoreData && scoreData.scoresRecent) {
-                        const { scoresRecent } = scoreData; 
-                        news.push(new News({                                        // Create a News with changes data of current bee
-                            text: `Your Bee ${userInfo.playerName} played ${numPlayedMore} new songs!`, 
-                            title: `Your Bee ${userInfo.playerName} played ${numPlayedMore} new songs!`,
-                            bee: userInfo.playerName,
-                            numPlayedMore,
-                            songs: scoresRecent.slice(0, numPlayedMore),
-                            type: "morePlayed",
-                            date: new Date().toISOString() 
-                        }))
-                        for (let j = 0; j < numPlayedMore; j++) {                           // For each score.... 
-                            const match = (myScoreIds.includes(scoresRecent[j].scoreId))    // check if song exist in our own Songlist
-                            if (match) {                                                    // if so...
-                                const { songName, songAuthorName, score } = scoresRecent[j]   // grab Song Infos
-
-                                // Get my score: filter my Songlist for same songId (can only be 1) and grab score
-                                // const myScore = myScoreIds.filter(item => item.scoreId === scoresRecent[j].scoreId).score 
-                                const myScore = 279100 //FIXME: For testing...
-                                if (score > myScore) {
-                                    news.push(new News({
-                                        text: `Your Bee ${playerName} beat you at ${songName} (${songAuthorName}): ${score} ( You: ${myScore}  ) `, 
-                                        type: "beatScore",
-                                        bee: playerName,
-                                        beeSocre: score,
-                                        myScore, 
-                                        song: scoresRecent[j], 
-                                        date: scoresRecent[j].timeSet
-                                    }))
-                                }                           
-                            }
-                        }
-                    }
-                    bees = bees.map(bee => (bee.playerId === playerId) ? bee = { ...userInfo, scoreData } : bee );
-                })
-        } 
-    }
-};
+  }
+}
--- client/src/reducer/rootReducer.js
+++ client/src/reducer/rootReducer.js
@@ -1,73 +1,80 @@
-import { 
-    ADD_NOTIFICATION, 
-    UPDATE_USER_DATA,
-    SET_FETCH_STATUS,
-    SET_LOGIN_STATUS,
-    LOGOUT,
-    LOGIN
-  } from '../actioncreators';
-  
-  const initialState = {
-      userdata: {},
-      notifications: [],
-      fetchingData: { 
-        status: false,
-        statusText: null
-      },
-      isLoggedIn: false,
-      loggingIn: false
-  }
-  
-  export default function rootReducer(state=initialState, action) {
-    let newState = { ...state }
-    switch(action.type) {
-  
-      case ADD_NOTIFICATION: {
-        return {
-          ...newState,
-          notifications: [...state.notifications, {
+import {
+  ADD_NOTIFICATION,
+  UPDATE_USER_DATA,
+  SET_FETCH_STATUS,
+  SET_LOGIN_STATUS,
+  LOGOUT,
+  LOGIN,
+} from "../actioncreators";
+
+const initialState = {
+  userdata: {},
+  notifications: [],
+  fetchingData: {
+    status: false,
+    statusText: null,
+  },
+  isLoggedIn: false,
+  loggingIn: false,
+};
+
+export default function rootReducer(state = initialState, action) {
+  let newState = { ...state };
+  switch (action.type) {
+    case ADD_NOTIFICATION: {
+      return {
+        ...newState,
+        notifications: [
+          ...state.notifications,
+          {
             notification: action.notification,
             typeOfNotification: action.typeOfNotification,
-            created: new Date().toISOString()
-          }]
-        }
-      }
-
-      case UPDATE_USER_DATA: {
-        return {
-          ...newState,
-          userdata: action.userdata
-        }
-      }
-
-      case SET_FETCH_STATUS: {
-        return {
-          ...newState,
-          fetchingData: {
-            status: action.fetchingData.status,
-            statusText: action.fetchingData.statusText
-          }
-        }
-      }
-
-      case SET_LOGIN_STATUS: {
-        return {
-          ...newState,
-          loggingIn: action.status,
-        }
-      }
-
-      case LOGIN: {
-        return {
-          ...newState,
-          isLoggedIn: true
-        }
-      }
-
-      case LOGOUT: {
-        return initialState
-      }
-      
-      default: return state;
+            created: new Date().toISOString(),
+          },
+        ],
+      };
     }
+
+    case UPDATE_USER_DATA: {
+      console.log(
+        "🚀 ~ file: rootReducer.js ~ line 41 ~ rootReducer ~ action.userdata",
+        action.userdata
+      );
+      return {
+        ...newState,
+        userdata: action.userdata,
+      };
+    }
+
+    case SET_FETCH_STATUS: {
+      return {
+        ...newState,
+        fetchingData: {
+          status: action.fetchingData.status,
+          statusText: action.fetchingData.statusText,
+        },
+      };
+    }
+
+    case SET_LOGIN_STATUS: {
+      return {
+        ...newState,
+        loggingIn: action.status,
+      };
+    }
+
+    case LOGIN: {
+      return {
+        ...newState,
+        isLoggedIn: true,
+      };
+    }
+
+    case LOGOUT: {
+      return initialState;
+    }
+
+    default:
+      return state;
   }
+}
